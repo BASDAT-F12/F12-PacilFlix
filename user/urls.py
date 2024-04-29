6549708a201df0ca1a381fbbb7f8c2bb9783b904
vinkakniv <vinka.aeris@gmail.com>
2024-04-29 18:24:07 +0700

    done implement cru langganan frontend

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user.views import list_package
+from user.views import list_package, buy_page
 
 app_name = 'user'
 
 urlpatterns = [
     path('daftar-langganan/', list_package, name='daftar-langganan'),
+    path('halaman-beli/', buy_page, name='halaman-beli')
 ]
