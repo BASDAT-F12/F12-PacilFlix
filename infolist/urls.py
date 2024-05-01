55fe16c723bb4cf2c5aa8071acfc6641cf88670d
farrelsheva <farrelshevaa@gmail.com>
2024-05-01 23:11:18 +0700

    scorch earth

--- infolist/urls.py
+++ infolist/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from infolist.views import list_tayangan
+from infolist.views import list_tayangan, detail_tayangan_episode,detail_tayangan_film,detail_tayangan_series, search_list
 
 app_name = 'infolist'
 
 urlpatterns = [
     path('list-tayangan/', list_tayangan, name='list-tayangan'),
+    path('search-tayangan/', search_list, name='search_list'),
+    path('detail-tayangan-film/', detail_tayangan_film, name='detail_tayangan_film'),
+    path('detail-tayangan-series/', detail_tayangan_series, name='detail_tayangan_series'),
+    path('detail-tayangan-episode/', detail_tayangan_episode, name='detail_tayangan_episode'),
 ]
--- infolist/views.py
+++ infolist/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import get_object_or_404,render
-from infolist.models import Tayangan, Film, Series, Sutradara
 
 def list_tayangan(request):
     # Random order
-    tayangans = Tayangan.objects.order_by('?')[:10]
-    films = Film.objects.all()
-    series = Series.objects.all()
-    return render(request, 'daftar-tayangan.html',{
-        'tayangans': tayangans,
-        'films': films,
-        'series': series
-    })
+    return render(request, 'daftar-tayangan.html')
     
-def detail_tayangan(request, tayangan_id):
-    tayangan = get_object_or_404(Tayangan, pk=tayangan_id)
-    if isinstance(tayangan, Film):
-        tayangan_type = 'film'
-    elif isinstance(tayangan, Series):
-        tayangan_type = 'series'
-    else:
-        return None
-    return render(request, 'detail-tayangan.html',{
-        'tayangan': tayangan,
-        'tayangan_type': tayangan_type
-        })
+def search_list(request):
+    return render(request, 'search-tayangan.html')
+
+def detail_tayangan_film(request):
+    return render(request, 'detail-tayangan-film.html')
+
+def detail_tayangan_series(request):
+    return render(request, 'detail-tayangan-series.html')
+    
+def detail_tayangan_episode(request):
+    return render(request, 'detail-tayangan-episode.html')
