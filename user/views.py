8bee84c333636069cf778daecb8ac2435ebaf810
vinkakniv <vinka.aeris@gmail.com>
2024-05-17 17:41:03 +0700

    implement cru langganan

--- user/views.py
+++ user/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
+from user.queries import get_all_packages, get_active_subscription, get_transaction_history, get_package_by_name
 
 # Create your views here.
 def list_package(request):
-    context = {
-        'name': 'PacilFix F12',
-        'class': 'BASDAT F'
-    }
-    return render(request, "daftar-langganan.html", context)
+    packages = get_all_packages()
+    active = get_active_subscription(request.session['username'])
+    histories = get_transaction_history(request.session['username'])
+    return render(request, "daftar-langganan.html", {
+        'packages': packages,
+        'active': active,
+        'histories': histories
+    })
 
-def buy_page(request):
+def buy_page(request, id):
+    buy_package = get_package_by_name(id)
     context = {
-        'name': 'PacilFix F12',
-        'class': 'BASDAT F'
+        'buy_package': buy_package
     }
     return render(request, "halaman-beli.html", context)
